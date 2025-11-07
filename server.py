b00f31e830e599105be4aa3e41624b03b412e40a
YBEC200 <160342571+YBEC200@users.noreply.github.com>
2025-11-07 13:21:55 -0500

    DA

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],    # en producción pon tu dominio específico
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
